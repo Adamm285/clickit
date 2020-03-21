b564c0fd9097e0ca1196edd7e0ff4d635a57b1f0
Adam m <adam@floorsmtihllc.com>
2020-03-21 13:16:11 -0400

    adjusting font

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <a className="navbar-brand fixed-bottom">Adam Murphy 2020</a>
+        </div>
+    )
+}
+
+export default Footer
